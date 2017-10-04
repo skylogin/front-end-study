55d6826b9f036da7aecfa8dd7db417168c3770b7
Chanho Jung <skylogin@gmail.com>
2017-10-05 00:15:30 +0900

    classify user email address (no duplication)

--- coffeerun/scripts/checklist.js
+++ coffeerun/scripts/checklist.js
@@ -4,6 +4,7 @@
   var App = window.App || {};
   var $ = window.jQuery;
 
+
   function CheckList(selector){
     if(!selector){
       throw new Error('No selector provided');
@@ -20,26 +21,63 @@
     'almond': '#0f0',
     'mocha': '#00f'
   };
-
+  var CLICKCOUNTER = 0;
 
   CheckList.prototype.addClickHandler = function(fn){
     this.$element.on('click', 'input', function(event){
-      var email = event.target.value;
-      this.removeRow(email);
-      fn(email);
+      CLICKCOUNTER++;
+      var self = this;
+
+      setTimeout(function(){
+        if(CLICKCOUNTER === 2){
+
+          //dbl 처리
+        } else if(CLICKCOUNTER === 1){
+          var email = event.target.value;
+          self.removeRow(email);
+          fn(email);
+        }
+        CLICKCOUNTER = 0;
+      },350)
+
     }.bind(this));
   };
   CheckList.prototype.addRow = function(coffeeOrder){
-    this.removeRow(coffeeOrder.emailAddress);
+    if(this.checkDupicationRow(coffeeOrder.emailAddress)){
+      this.removeRow(coffeeOrder.emailAddress);
+    } else{
+      this.completeRow(coffeeOrder.emailAddress);
+    }
 
     var rowElement = new Row(coffeeOrder);
     this.$element.append(rowElement.$element);
   };
-  CheckList.prototype.removeRow = function(email){
+  CheckList.prototype.checkDupicationRow = function(email){
+    var disabledFlag = this.$element.find('[value="' + email + '"]').attr('disabled');
+    var usedFlag = this.$element.find('[value="' + email + '"]').attr('data-coffee-used');
+
+    if(disabledFlag === undefined && usedFlag === 'true'){
+      return true;
+    } else{
+      return false;
+    }
+
+  };
+  CheckList.prototype.completeRow = function(email){
     this.$element
       .find('[value="' + email + '"]')
       .closest('[data-coffee-order="checkbox"]')
       .remove();
+
+  };
+  CheckList.prototype.removeRow = function(email){
+    this.$element
+      .find('[value="' + email + '"]')
+      .attr('disabled', 'disabled')
+      .attr('data-coffee-used', 'true')
+      .parent()
+      .css('text-decoration','line-through')
+      .css('color', '#aaa');
   };
 
   function Row(coffeeOrder){
@@ -52,7 +90,8 @@
 
     var $checkbox = $('<input></input>', {
       type: 'checkbox',
-      value: coffeeOrder.emailAddress
+      value: coffeeOrder.emailAddress,
+      'data-coffee-used': 'false'
     });
 
     var description = ' [' + coffeeOrder.strength + 'x]';
