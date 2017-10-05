ab4493073f21d2dab1f2231695d1b4006e1edd78
Chanho Jung <skylogin@gmail.com>
2017-10-05 12:15:23 +0900

    add dbl click event

--- coffeerun/scripts/checklist.js
+++ coffeerun/scripts/checklist.js
@@ -30,15 +30,28 @@
 
       setTimeout(function(){
         if(CLICKCOUNTER === 2){
+          //dbl클릭 처리
+          var data = event.target.nextSibling.data;
+          var tempData = data.split(',');
+          var orderData = tempData[0].split(' ');
+          var userData = tempData[1].split(' ');
 
-          //dbl 처리
+          var ratingData = orderData[0].split('[');
+          var rating = ratingData[1].split(']')[0];
+          var size = orderData[1];
+          var flavor = orderData[2] || '';
+          var order = userData[0];
+          var email = event.target.value;
+
+          modifyOrder(order, email, size, flavor, rating);
         } else if(CLICKCOUNTER === 1){
+          //기존 one클릭 처리
           var email = event.target.value;
           self.removeRow(email);
           fn(email);
         }
         CLICKCOUNTER = 0;
-      },350)
+      },350);
 
     }.bind(this));
   };
@@ -68,7 +81,6 @@
       .find('[value="' + email + '"]')
       .closest('[data-coffee-order="checkbox"]')
       .remove();
-
   };
   CheckList.prototype.removeRow = function(email){
     this.$element
@@ -94,13 +106,13 @@
       'data-coffee-used': 'false'
     });
 
-    var description = ' [' + coffeeOrder.strength + 'x]';
-    description += coffeeOrder.size + ' ';
+    var description = '[' + coffeeOrder.strength + '] ';
+    description += coffeeOrder.size;
     if(coffeeOrder.flavor){
-      description += coffeeOrder.flavor + ' ';
+      description += ' ' + coffeeOrder.flavor;
       $label.css('color', FLAVOR[coffeeOrder.flavor]);
     }
-    description += coffeeOrder.coffee + ', ';
+    description += ',' + coffeeOrder.coffee;
     description += ' (' + coffeeOrder.emailAddress + ')';
 
     $label.append($checkbox);
@@ -108,6 +120,16 @@
     $div.append($label);
 
     this.$element = $div;
+  }
+
+  function modifyOrder(order, email, size, flavor, rating){
+    $('#coffeeOrder').val(order);
+    $('#emailAddress').val(email);
+    $('input:radio[name="size"][value="' + size + '"]').prop('checked', true);
+    $('#flavorShot').val(flavor);
+    $('#strengthLevel').val(rating);
+    $('#strengthValue').html(rating);
+
   }
 
   App.CheckList = CheckList;
