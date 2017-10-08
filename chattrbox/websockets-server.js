5174c014b747dc6aa674826c927196293536b8f3
Chanho Jung <skylogin@gmail.com>
2017-10-08 15:36:30 +0900

    add user authentication

--- chattrbox/websockets-server.js
+++ chattrbox/websockets-server.js
@@ -4,23 +4,42 @@ var port = 3001;
 var ws = new WebSocketServer({
   port: port
 });
+
 var messages = [];
+var password = 'Wsordfish';
+var users = [];
+var id = 0;
 
 console.log('websockets server started');
 
-
 ws.on('connection', function(socket){
   console.log('client connection established');
-
-  messages.forEach(function(msg){
-    socket.send(msg);
-  })
+  var user = {};
+  user.id = ++id;
+  user.auth = false;
+  users.push(user);
+  socket.userId = id;
+  socket.send('enter your password: ');
 
   socket.on('message', function(data){
     console.log('message received: ' + data);
-    messages.push(data);
+
+    if(data === password){
+      users[socket.userId-1].auth = true;
+      messages.forEach(function(msg){
+        socket.send(msg);
+      });
+    } else{
+      if(!users[socket.userId-1].auth){
+        socket.send('wrong password');
+      }
+    }
+
     ws.clients.forEach(function(clientSocket){
-      clientSocket.send(data);
+      if(socket.userId === users[socket.userId-1].id && users[socket.userId-1].auth){
+        clientSocket.send(data);
+        messages.push(data);
+      }
     });
   });
 });
