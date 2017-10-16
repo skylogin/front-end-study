d1ba87b44aee282d420da44d5b37ac43b71f9d1b
Chanho Jung <skylogin@gmail.com>
2017-10-16 23:22:44 +0900

    add routes, link

--- tracker/app/routes/cryptids.js
+++ tracker/app/routes/cryptids.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel(){
-    this.store.createRecord('cryptid', {
-      "name": "Charlie",
-      "cryptidType": "unicorn"
-    });
-  },
   model(){
     return this.store.findAll('cryptid');
   }
--- tracker/ember-cli-build.js
+++ tracker/ember-cli-build.js
@@ -7,10 +7,11 @@ module.exports = function(defaults) {
   var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
 
   var app = new EmberApp(defaults, {
-      
+
   });
 
   app.import(bootstrapPath + 'javascripts/bootstrap.js');
-
+  app.import('bower_components/moment/moment.js');
+  
   return app.toTree();
 };
