f2485c86809eb5cee5e59d1b1115eb123ed0e788
Chanho Jung <skylogin@gmail.com>
2017-10-17 00:02:18 +0900

    route & controller

--- tracker/app/router.js
+++ tracker/app/router.js
@@ -10,10 +10,10 @@ Router.map(function() {
     this.route('new');
   });
   this.route('sighting', function() {
-    this.route('edit');
+    this.route('edit', {path: "sightings/:sighting_id/edit"});
   });
   this.route('cryptids');
-  this.route('cryptid', {path: 'cryptids/:cryptid_id'});
+  this.route('cryptid', {path: "cryptid/:cryptid_id"});
   this.route('witnesses');
   this.route('witness');
 });
